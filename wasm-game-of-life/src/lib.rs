ba38b9134a490789ed05a3a471a88d7e208fc361
Jang Haemin <io@jhaemin.com>
2021-06-26 23:53:16 +0900

    Modify greet function to take a parameter

--- wasm-game-of-life/src/lib.rs
+++ wasm-game-of-life/src/lib.rs
@@ -9,11 +9,14 @@ use wasm_bindgen::prelude::*;
 static ALLOC: wee_alloc::WeeAlloc = wee_alloc::WeeAlloc::INIT;
 
 #[wasm_bindgen]
-extern {
+extern "C" {
     fn alert(s: &str);
 }
 
 #[wasm_bindgen]
-pub fn greet() {
-    alert("Hello, wasm-game-of-life!");
+pub fn greet(name: &str) {
+    let mut hello = "Hello, ".to_owned();
+    hello.push_str(name);
+
+    alert(hello.as_str());
 }
